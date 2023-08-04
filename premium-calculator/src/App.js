794f2d815629a28713a5d48dd1df0c6c472d03d0
Rutwik1801 <65919321+Rutwik1801@users.noreply.github.com>
2023-08-04 15:07:59 +0530

    Update App.js

--- premium-calculator/src/App.js
+++ premium-calculator/src/App.js
@@ -32,7 +32,7 @@ function App() {
     const childAgesArray = childAges.map((child) => parseInt(child.age));
 
     // Send user input to the backend to calculate premium
-    axios.post('https://premium-calculator-sstf.vercel.app/calculate_premium', {
+    axios.post('https://flask-production-21cd.up.railway.app/calculate_premium', {
       plan: plan,
       tier: parseInt(tier.split("-")[1]),
       sumInsured: parseInt(sumInsured),
